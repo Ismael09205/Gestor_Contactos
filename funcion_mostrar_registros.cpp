07e98c378fa1a2e9c1e90da14aa26eae53b2f304
emely711 <maite.bone02@gmail.com>
2025-07-21 15:53:00 -0500

    Update funcion_mostrar_registros.cpp

--- funcion_mostrar_registros.cpp
+++ funcion_mostrar_registros.cpp
@@ -1,1 +1,34 @@
-namespace
+#include<iostream>
+#include<string>
+#include<fstream>
+
+using namespace std;
+
+void mostrar_contactos(){
+	ifstream archivo_contactos("lista_contactos.txt");
+	string linea;
+	
+	cout<<"\n--- Contactos Registrados --\n";
+	while (getline(archivo_contactos, linea)){
+		size_t pos = 0;
+		string datos [7];
+		int i = 0;
+		
+		while ((pos = linea.find("|")) != string::npos && i < 7){
+			datos[i++] = linea. substr(0, pos);
+			linea.erase(0, pos+1);
+		}
+		datos [i] = linea;
+		
+		cout<<"Nombre: "<<datos [0]<<endl;
+		cout<<"Apellido: "<<datos [1]<<endl;
+		cout<<"Telefono: "<<datos [2]<<endl;
+		cout<<"Correo: "<<datos [3]<<endl;
+		cout<<"Direccion: "<<datos [4]<<endl;
+		cout<<"Apodo: "<<datos [5]<<endl;
+		cout<<"Cumpleaños: "<<datos [6]<<endl;
+		cout<<"--------------------------\n";	
+	}
+	archivo_contactos.close();
+	
+}
