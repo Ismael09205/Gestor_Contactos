0dda387034beafbf79216cc3128510a73db8ba97
emely711 <maite.bone02@gmail.com>
2025-07-22 20:17:52 -0500

    Update funcion_mostrar_registros.cpp

--- funcion_mostrar_registros.cpp
+++ funcion_mostrar_registros.cpp
@@ -1,34 +1,25 @@
 #include<iostream>
-#include<string>
 #include<fstream>
+#include<string>
 
 using namespace std;
-
-void mostrar_contactos(){
-	ifstream archivo_contactos("lista_contactos.txt");
+void mostrarContactos(){
+	ifstream archivo("lista_contactos.txt");
 	string linea;
-	
-	cout<<"\n--- Contactos Registrados --\n";
-	while (getline(archivo_contactos, linea)){
-		size_t pos = 0;
-		string datos [7];
-		int i = 0;
-		
-		while ((pos = linea.find("|")) != string::npos && i < 7){
-			datos[i++] = linea. substr(0, pos);
-			linea.erase(0, pos+1);
+	int num = 1;
+
+	if(archivo.is_open()){
+		cout<<"\nLista de Contactos: ";
+		while (getline(archivo, linea)){
+			cout<<num++ <<". "<<linea<<"\n";
 		}
-		datos [i] = linea;
-		
-		cout<<"Nombre: "<<datos [0]<<endl;
-		cout<<"Apellido: "<<datos [1]<<endl;
-		cout<<"Telefono: "<<datos [2]<<endl;
-		cout<<"Correo: "<<datos [3]<<endl;
-		cout<<"Direccion: "<<datos [4]<<endl;
-		cout<<"Apodo: "<<datos [5]<<endl;
-		cout<<"Cumpleaños: "<<datos [6]<<endl;
-		cout<<"--------------------------\n";	
+		archivo.close();
+	}else{
+		cout<<"No se pudo abrir el archivo con los registros.\n";
 	}
-	archivo_contactos.close();
-	
+}
+
+int main(){
+	mostrarContactos();
+	return 0;
 }
